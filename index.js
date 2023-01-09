8dd45167e5a72df7f84750bc177f87d6896fdbb6
Justin Verthein <Justinmackay88@hotmail.com>
2023-01-09 12:43:39 -0800

    complete lab

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
+{
+let num1 = 2
+let num2 = 31
+let multiply = num1 * num2 }
 
-// Write your code here
+var random = Math.floor(Math.random() * 10) + 1;
+
+{
+let num3 = 18
+let num4 = 7
+const mod = num3 % num4;
+}
+
+let max = 20
+math.max(1, 2, 10, 20)
